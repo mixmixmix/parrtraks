ef18fdf193a758f45466dacbbe9343a7270fc9d4
Mixmixmix <mikolaj.kundegorski@gmail.com>
2019-07-06 22:52:44 +0100

    improvements

--- parrtrak.py
+++ parrtrak.py
@@ -4,7 +4,9 @@ Assumptions:
 - recordings done at 10fps
 
 """
-import cv2
+import signal
+import cv2, yaml
+import sys, argparse
 import numpy as np
 from datetime import datetime, timedelta
 
@@ -31,71 +33,141 @@ def ripFname(fname):
     ripped_date = datetime(year, month, day, hour, minute, 30)
     return ripped_date
 
-
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-# fgbg = cv2.createBackgroundSubtractorMOG()
-
-fname = '2019mar29_1800_23h.h264'
-
-start_date = ripFname(fname)
-current_date = start_date
-ctime = current_date.strftime("%I:%M%:%Sp")
-fps = 10
+brexit = 0
+def signal_handler(sig, frame):
+        global brexit
+        print('You pressed Ctrl+C!')
+        brexit = 1
+        print(brexit)
 
 
-cap = cv2.VideoCapture(fname)
+def main(args):
+    signal.signal(signal.SIGINT, signal_handler)
+    cv2.namedWindow('frame', cv2.WINDOW_GUI_EXPANDED)
+    cv2.moveWindow('frame', 20,20)
+    cv2.namedWindow('fg', cv2.WINDOW_GUI_EXPANDED)
+    cv2.moveWindow('fg', 20,20)
 
-# cap.set(cv2.CAP_PROP_FPS, 30)
-# fps = cap.get(cv2.CAP_PROP_FPS)
-fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+    # fgbg = cv2.bgsegm.createBackgroundSubtractorCNT()
+    # fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+    # fgbg = cv2.createBackgroundSubtractorMOG()
 
-print(fps)
-print("No of frames:")
-print(fcount)
+    # fname = '2019mar29_1800_23h.h264'
+    # fname = '2019apr14_0008_unknown.h264'
+    # fname = '2019mar20_0905_4h.h264'
+    # fname = '2019may02_2118_all.h264'
+    fname = args.fname[0]
+    # fname = '2019apr27_1131_groupA9.mp4'
+    print(fname)
+    start_date = ripFname(fname)
+    current_date = start_date
+    ctime = current_date.strftime("%I:%M%:%Sp")
+    fps = 8
 
-# Check if camera opened successfully
-if (cap.isOpened() == False):
-    print("Error opening video stream or file")
 
-ret, frame = cap.read()
-w = frame.shape[1]
-h = frame.shape[0]
 
-print("Image shape is ")
-print(frame.shape)
 
-# Read until video is completed
-while (cap.isOpened()):
-    # Capture frame-by-frame
+    cap = cv2.VideoCapture(fname)
+
+    # cap.set(cv2.CAP_PROP_FPS, 30)
+    # fps = cap.get(cv2.CAP_PROP_FPS)
+    fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    print(fps)
+    print("No of frames:")
+    print(fcount)
+
+    # Check if camera opened successfully
+    if (cap.isOpened() == False):
+        print("Error opening video stream or file")
+
     ret, frame = cap.read()
-    if ret == True:
-        fgmask = fgbg.apply(frame)
-        cv2.imshow('Background mask',fgmask)
-        current_date = current_date + timedelta(seconds=0.1)
-        ctime = current_date.strftime("%I:%M%:%Sp")
-        # Display the resulting frame
-        cv2.putText(frame, ctime,  (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-        cv2.imshow('Frame', frame)
-        #depressedKey = cv2.waitKey(1000//fps)
-        depressedKey = cv2.waitKey(40)
-        if depressedKey == 'q':
+    iterator = 1
+    w = frame.shape[1]
+    h = frame.shape[0]
+
+    print("Image shape is ")
+    print(frame.shape)
+
+    noPixels = h * w
+    movement = []
+    # Read until video is completed
+    i=0
+    while (cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        i=i+1
+        if ret == True and brexit==0:
+            iterator=iterator+1
+            fgmask = fgbg.apply(frame)
+            current_date = current_date + timedelta(seconds=(1/fps))
+            ctime = current_date.strftime("%I:%M%:%Sp")
+            # Display the resulting frame
+            if args.visual:
+                cv2.imshow('fg',fgmask)
+                cv2.putText(frame, ctime,  (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
+                cv2.imshow('frame', frame)
+                key = cv2.waitKey(int(1000/fps))
+                if key == ord('q'):
+                    print("quit")
+                    break
+            if args.save:
+                if (iterator % 100 == 0):
+                    cv2.imwrite('may'+str(iterator)+'f.png', frame)
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
+
+            ones = cv2.countNonZero(fgmask)
+            coverage = 100 * (ones/noPixels)
+            # print("{0} : {1:.2f} ".format(ctime, coverage ))
+            movement.append([ctime,coverage])
+            sys.stdout.write('\r')
+            sys.stdout.write("[%-20s] %d%% %d/%d" %
+                                ('=' * int(20 * i / float(fcount)),
+                                 int(100.0 * i / float(fcount)), i, fcount))
+            #depressedKey = cv2.waitKey(1000//fps)
+            # depressedKey = cv2.waitKey(40)
+            # if depressedKey == 'q':
+                # break
+
+            # processing
+            # M = cv2.getRotationMatrix2D((w/2,h/2), 30, 1)
+            # rotated = cv2.warpAffine(frame, M, (w, h))
+            # cropped = rotated[200:400, 200:400]
+
+            # cv2.imshow('Rotated',rotated)
+            # cv2.imwrite( "rotated.jpg", rotated )
+            # cv2.imwrite( "cropped.jpg", cropped )
+
+            # Press Q on keyboard to    exit
+        else:
             break
 
-        # processing
-        # M = cv2.getRotationMatrix2D((w/2,h/2), 30, 1)
-        # rotated = cv2.warpAffine(frame, M, (w, h))
-        # cropped = rotated[200:400, 200:400]
+    # When everything done, release the video capture object
+    cap.release()
 
-        # cv2.imshow('Rotated',rotated)
-        # cv2.imwrite( "rotated.jpg", rotated )
-        # cv2.imwrite( "cropped.jpg", cropped )
+    # Closes all the frames
+    cv2.destroyAllWindows()
 
-        #cv2.waitKey(0)
-        # break
-        # Press Q on keyboard to    exit
+    with open(args.output[0], 'w') as handle:
+        yaml.dump(movement, handle)
 
-# When everything done, release the video capture object
-cap.release()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=
+        'Analyse videos of migrating fish',
+        epilog=
+        'Any issues and clarifications: github.com/mixmixmix/parrtrak/issues')
+    parser.add_argument(
+        '--fname', '-f', required=True, nargs=1, help='filename')
+    parser.add_argument(
+        '--output', '-o', required=True, nargs=1, help='outputfile')
+    parser.add_argument('--visual', '-v', default=False, action='store_true',
+                        help='Display tracking progress')
+    parser.add_argument('--save', '-s', default=False, action='store_true',
+                        help='save every 100th frame')
 
-# Closes all the frames
-cv2.destroyAllWindows()
+
+    args = parser.parse_args()
+    main(args)
